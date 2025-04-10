f2fae5e79bb6308a146087dd882afdbdc3f66b79
Gagandeep Singh Makhija <gagandeep.2020@gmail.com>
2025-04-10 15:26:41 +0530

    Bumping up release for cli fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="email_deliverability",
-    version="0.1.2",
+    version="0.1.3",
     description="Comprehensive email deliverability management tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
